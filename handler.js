bc1c2f0b4b933caa20728e79ac6e0ae78e440636
sravanigumma <134568373+sravanigumma@users.noreply.github.com>
2023-07-18 05:49:35 +1000

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,15 +1,18 @@
-const AWS = require('aws-lambda');
+'use strict';
 
-exports.handler = async (event) => {
+module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Test lambda function executed successfully!',
+        message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
       2
-     ),
-    };
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
